e5471b6785b9c70b6b6e3d9bc9bda64354fc7964
purvik19 <purvikmepani18@gmail.com>
2025-03-17 10:49:53 +0530

    Update

--- Weather App/src/hook/customehook.js
+++ Weather App/src/hook/customehook.js
@@ -1,7 +1,7 @@
 
 export const handleSearch = async (city,setWeatherData) => {
     try {
-        const apiKeyall = "5194e84f0f809ca64d41719d9f54a31b"; // Get the API key from the environment variable
+        const apiKeyall = "Your api"; // Get the API key from the environment variable
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKeyall}`);
         const data = await response.json();
         setWeatherData(data)
